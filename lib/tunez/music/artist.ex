8be72e1f8b4075ca15bd2f4f1604053ad33dc3c8
Francesco <francescobutano@gmail.com>
2025-12-13 20:13:41 +0100

    aggiunto attributo previous_names a Artist e aggiunto change che salva i nomi precedenti ad ogni cambio di nome

--- lib/tunez/music/artist.ex
+++ lib/tunez/music/artist.ex
@@ -21,13 +21,24 @@ defmodule Tunez.Music.Artist do
 
     attribute :biography, :string
 
+    attribute :previous_names, {:array, :string} do
+      default []
+    end
+
     create_timestamp :inserted_at
     update_timestamp :updated_at
   end
 
 
   actions do
-    defaults [:create, :read, :update, :destroy]
+    defaults [:create, :read, :destroy]
     default_accept [:name, :biography]
+
+    update :update do
+      require_atomic? false
+      accept [:name, :biography]
+      change Tunez.Music.Changes.UpdatePreviousNames, where: [changing(:name)]
+    end
+
   end
 end
