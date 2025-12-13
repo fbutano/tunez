9427d895edfefef967b57525a36ace0275edab5d
Francesco <francescobutano@gmail.com>
2025-12-13 19:53:56 +0100

    aggiunto vincolo identità artista - nome album

--- lib/tunez/music/album.ex
+++ lib/tunez/music/album.ex
@@ -54,6 +54,11 @@ defmodule Tunez.Music.Album do
       message: "must start with https:// or /images/"
   end
 
+  identities do
+    identity :unique_album_names_per_artist, [:name, :artist_id],
+    message: "already exists for this artist"
+  end
+
   def next_year, do: Date.utc_today().year + 1
 
 end
